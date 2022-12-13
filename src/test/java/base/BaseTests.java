84896f6edf72baa35437532ade1d7d8d25dc579b
Kholofelo-digilink <Kholofelo@digilink.africa>
2022-12-13 09:16:41 +0200

    git commit -m "in progress"

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,7 +1,12 @@
 package base;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class BaseTests {
 
@@ -12,7 +17,25 @@ public class BaseTests {
         driver = new ChromeDriver();
         driver.get("https://www.saucedemo.com");
 
+        //finding elements
+        List<WebElement> links = driver.findElements(By.tagName("a"));
+        System.out.println(links.size());
+
+        WebElement inputLink = driver.findElement(By.linkText("Angie"));
+        inputLink.click();
+
+
+        driver.findElement(By.id("first-name")).sendKeys("Nonhlanhla");
+        //or
+        driver.findElement(By.id("continue")).click();
+
+        //manage the window display
+        driver.manage().window().maximize();;
+        driver.manage().window().fullscreen();
+        driver.manage().window().setSize(new Dimension(375, 812));
+
         System.out.println(driver.getTitle());
+        driver.quit();
     }
     public static void main(String args[]){
         BaseTests test = new BaseTests();
